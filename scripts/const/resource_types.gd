4e7b017fbe5df783d5da5260211b1fc38914aa1d
madokakaname345 <podori.seru@gmail.com>
2025-03-11 22:27:18 +0900

    add resources

--- scripts/const/resource_types.gd
+++ scripts/const/resource_types.gd
@@ -1,3 +1,8 @@
 extends Node
 
+var FOOD = "food"
+var CLOTHES = "clothes"
 var IRON_ORE = "iron_ore"
+
+
+enum PRODUCTION_TYPE {NONE, MANUAL_LABOR, TRANSPORTATION, INTELLECTUAL_LABOR, ORGANIZATIONAL}
